3a2d77b6dfa0ddf2b43e85dd1f92351ea82d6f2c
Josh <josh.s.brown@hotmail.com>
2012-04-27 21:33:35 -0400

    With Comments!! Also, added a line to account for double letters appearing in words. The program is now a fully operational battle station!! My first one!

--- hangman.py
+++ hangman.py
@@ -4,12 +4,18 @@
 #generate a random number, which will pull from a dictionary
 #to use the word for the hangman program.
 import random
+#imports the string module, which allows me to use the Len() function to determine the length of the word chosen
 import string
 #this is the key (dictionary) we will use to pull words for our program.
 dictionary={1:"stinkface", 2:"couches", 3:"blues", 4:"porkface", 5:"keyboard", 6:"computer", 7:"whales", 8:"catscratch", 9:"woman", 10:"mankind"}
+#picks a random number between 1 and 10 that we will use to 
+#call the word
 choice=random.randint(1, 10)
+#the variable 'word' as the word that corresponds to the number from the previous line
 word=dictionary[choice]
-print "ANSWER:", word
+#This is just a cheat for testing purposes. Uncomment the next line to see the word at the beginning of the program.
+#print "ANSWER:", word
+#title page
 print '''
 
 
@@ -24,14 +30,18 @@ WELCOME TO JOSHs HANGING OF THE MEN!!
 
 
 '''
+#the following three lines prints one blank for each letter in the word, giving you a starting point, followed by an empy line (the "\n").
 for x in word:
  print "_",
 print "\n"
+#the next three strings are set to empty strings, and will be used to store the user's input.
 choices=""
 badchoices=""
-hangman=1
 goodchoices=""
-win=1
+#'hangman' is a counter that will be used to display the right hangman
+hangman=1
+#win is the counter to determine when the player wins.
+win=0
 head= """\n\n\nHangman:\n
 0 \n\n\n\n"""
 body= """\n\n\nHangman:\n\n 0
@@ -55,25 +65,39 @@ ALLDONE="""\n\n\nHangman:\n\n 0
 \n\n\n\n****GAME OVER!!!!****\n******YOU LOSE!!******\n\n\n\n
 """
 
-
-
+#this is the main function of the program. it accepts input from the user
+#and sets it to "pickletter". 
 def letterentry():
  global pickletter
  pickletter=raw_input("\nEnter a letter or hit CTRL+Z to exit: ")
  global choices
+#the following three lines keeps the user from entering a character
+#they have already entered, or from entering a space, or from entering
+#more than one character in one turn
  while pickletter in choices or len(pickletter)!=1 or pickletter==" ":
   print "Try another letter please."
   pickletter=raw_input("\nEnter a letter or hit CTRL+Z to exit: ")
+#the next three lines assigns the user's input to the 'choices' variable
+#and then runs a check on it (which will be explained further down).
  if pickletter not in choices:
   choices = choices + pickletter
   checks(pickletter)
+#if the input is not in the word, calls the badmove function, basically
+#increasing the amount of the hangman you have
   if pickletter not in word:
    badmove()
+#if input is in the word, this function (goodmove()) takes you one step closer to winning.
   else:
    goodmove()
+#the following two lines print a summary of the letters you've chosen
+#divided into good choices and bad choices
  print "\nTHESE LETTERS ARE NOT IN THE WORD:", badchoices, "\n"
  print "THESE ARE GOOD LETTERS:", goodchoices, "\n\n", "==="*15
 
+#this function adds a letter to the 'goodchoices' variable, 
+#then prints out the word with the letters you've guessed 
+#correctly printed out and the letters you havent guessed
+#are printed as a blank
 def checks(item):
  print "\n"
  for x in word:
@@ -87,23 +111,26 @@ def checks(item):
    print "_",
  print "\n"
 
+#this function increases the 'win' counter by 1 for each correct letter you've guessed.
 def goodmove():
  global win
  global pickletter
  global choices
- count=0
-# if pickletter in word:
-#  win=win+1
-# for x in word:
-#  if x==pickletter:
-#   count=count+1
-#   win=win+1
- if pickletter in word:
-  win=win+1
- if win==len(word)+1:
+#this line was tricky for me. it goes through each letter in the word
+#each turn and for each time the letter you guessed appears in the letter
+#the 'win' counter is increased by one. (this is what
+#makes words with double letters winnable)
+ for x in word:
+  if pickletter == x:
+   win=win+1
+#once you've guessed the right letters, you win!
+ if win==len(word):
   print "\n\n\n**** YOU WIN!! ****\n\n\n"
   return
 
+#for each time you guess a letter that's not in the word,
+#this function increases the 'hangman' counter by one, bringing
+#you closer to your lynching!
 def badmove():
   global badchoices
   global choices
@@ -124,5 +151,9 @@ def badmove():
    print ALLDONE
   hangman=hangman+1
 
-while hangman<=6 and win<=len(word):
+#as long as you haven't reached the proper counters for 'hangman'
+#and 'win', this statement calls the letterentry() function.
+#not sure why though the win has to be less than or 
+#equal to the length of the word -1. oh well.
+while hangman<=6 and win<=len(word)-1:
  letterentry()
